bdb01d2aa1bd77f7c3f80ead0111df0d72b5818f
Lorand Petrindean <lorandp2007@gmail.com>
2025-03-06 13:40:37 +0000

    general cleanup

--- shaders/draw-vs.glsl
+++ shaders/draw-vs.glsl
@@ -1,4 +1,4 @@
-#version 450
+#version 330 core
 
 layout (location = 0) in vec3 aVPos;
 layout (location = 1) in vec4 aColour;
@@ -10,7 +10,6 @@ out vec4 vPos;
 out vec4 vColour;
 
 void main(void) {
-    vPos = uMVMatrix * vec4(aVPos, 1.0);
-    gl_Position = uPMatrix * vPos;
+    gl_Position = vec4(aVPos, 1.0);
     vColour = aColour;
 }
--- shaders/vs.glsl
+++ shaders/vs.glsl
@@ -1,4 +1,4 @@
-#version 450
+#version 330 core
 
 layout (location = 0) in vec3 aVPos;
 layout (location = 1) in vec2 aTexCoord;
